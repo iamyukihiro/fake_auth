f816d880d5b2a27ba8180857068d20440ea3447d
iamyukihiro <goreboothero@gmail.com>
2022-08-17 16:14:23 +0900

    ログイン/ログアウトできることを確認する機能テストを追加

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SecurityControllerTest extends WebTestCase
 {
@@ -51,5 +52,27 @@ class SecurityControllerTest extends WebTestCase
 
         $response = $this->client->getResponse();
         $this->assertTrue($response->isRedirect(), (string) $response->getStatusCode());
+    }
+
+    public function test_ログインできること(): void
+    {
+        $this->markTestSkipped('クローラーでFormにUsernameを入力して、/admin/dashboard へ遷移するテストを書く');
+    }
+
+    public function test_ログアウトできること(): void
+    {
+        $router = $this->client->getContainer()->get('router');
+        $this->client->followRedirects();
+
+        $user = $this->client->getContainer()->get('doctrine')
+            ->getRepository(User::class)
+            ->findOneBy(['username' => 'test@example.com'])
+        ;
+        $this->client->loginUser($user);
+        $crawler = $this->client->request('GET', '/logout');
+
+        $response = $this->client->getResponse();
+        $this->assertTrue($response->isOk(), (string) $response->getStatusCode());
+        $this->assertSame($router->generate('app_landing_page', [], UrlGeneratorInterface::ABSOLUTE_URL), $crawler->getUri());
     }
 }
