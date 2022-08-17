23f225ad948a44879df9ed421dd7101b33d6139d
iamyukihiro <goreboothero@gmail.com>
2022-08-17 09:54:11 +0900

    [DX]cs-fixを実行できるように

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,20 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('public')
 ;
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
-        '@Symfony' => true,
+        '@PSR2' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'php_unit_construct' => true,
+        'php_unit_strict' => true,
+        'array_indentation' => true,
+        'global_namespace_import' => true
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
